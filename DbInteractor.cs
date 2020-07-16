fdc1eec977461629485f974ec39e14710530399b
JonyElya <38189777+JonyElya@users.noreply.github.com>
2020-07-17 00:41:49 +0700

    initialize project

---
+++ DbInteractor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Back_Wiki
+{
+    public class DbInteractor : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+            .UseSqlite(@"Data Source = Articles.db;");
+        }
+        public DbSet<Articles> Articles { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Articles>().HasKey(m => m.Id);
+        }
+    }
+    
+}
---
+++ Migrations/20200716173138_ArticlesMigration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Back_Wiki.Migrations
+{
+    public partial class ArticlesMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    title = table.Column<string>(nullable: true),
+                    snippet = table.Column<string>(nullable: true),
+                    pageId = table.Column<int>(nullable: false),
+                    timestamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Articles",
+                columns: new[] { "Id", "pageId", "snippet", "timestamp", "title" },
+                values: new object[] { 1L, 222, "awdasdasdas", new DateTime(2020, 7, 16, 17, 31, 37, 577, DateTimeKind.Utc).AddTicks(7811), "title" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articles");
+        }
+    }
+}
